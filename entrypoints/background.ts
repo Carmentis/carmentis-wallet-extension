f61056fc03392c6da16d631a6bb9d5848ccf67a5
Selim TAVUKCUOGLU <selim@tavux.tech>
2024-05-13 12:34:31 +0200

    Clean up code

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,34 +1,15 @@
-import {useWallet} from "@/hooks/useWallet.tsx";
-import {CarmentisProvider} from "@/providers/CarmentisProvider.ts";
 import {browser} from "wxt/browser";
 
 
 export default defineBackground({
     persistent: true,
     main() {
-        /*browser.browserAction.openPopup().then(() => {
-            //console.log("Popup ouvert");
-        });*/
         console.log('Hello background!', {id: browser.runtime.id});
-
-        /*browser.runtime.onMessageExternal.addListener(async (message) => {
-            console.log("Message reçu in background :", message)
-            // Assurez-vous que le message vient de la source attendue
-            if (message === "openPopupRequest") {
-                browser.browserAction.openPopup().then(() => {
-                    console.log("Popup ouvert");
-                });
-                return 'Popup ouvert';
-            }
-            return 'Message non reconnu';
-        });*/
 
         browser.runtime.onConnect.addListener((port) => {
             port.onMessage.addListener((message) => {
                 console.log('Background received:', message);
 
-                //console.log('Background sending:', 'pong');
-                //port.postMessage('pong');
                 if (message.event === "openPopupRequest") {
                     console.log("Popup request")
                     browser.windows.create({
@@ -46,14 +27,8 @@ export default defineBackground({
             });
         });
 
-
-            browser.runtime.onMessage.addListener( (message) => {
+        browser.runtime.onMessage.addListener( (message) => {
             console.log("Message reçu in bg :", message)
-            // Assurez-vous que le message vient de la source attendue
-            /*browser.runtime.sendMessage(event.data).then((response) => {
-                console.log('Popup response:', response);
-            });*/
-            alert("Message reçu in bg :" + message)
         });
     }
 });
