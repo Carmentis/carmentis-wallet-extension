4fd3e6ac02478264ba26fa8563de29fc4bcd4a65
Selim TAVUKCUOGLU <selim@tavux.tech>
2024-09-04 15:16:38 +0200

    BIG CHANGES IN CSS

--- entrypoints/main/main.js
+++ entrypoints/main/main.js
@@ -1,7 +1,5 @@
-import './style.css';
 import '@/public/assets/vendor/bootstrap-icons/bootstrap-icons.css';
 import * as CarmentisApp from "../../lib/carmentis-app.js"
-import {browser} from "wxt/browser";
 
 console.log("Carmentis Wallet initializing...");
 CarmentisApp.initialize().then(r => {
--- entrypoints/popup/popup.js
+++ entrypoints/popup/popup.js
@@ -1,6 +1,3 @@
-import './style.css';
-import '@/public/assets/vendor/bootstrap-icons/bootstrap-icons.css';
-import * as CarmentisApp from "../../lib/carmentis-app.js"
 import {browser} from "wxt/browser";
 
 browser.tabs.create({
