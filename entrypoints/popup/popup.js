d57735d21e3cbac653829eb221d2da8527599dc7
Selim TAVUKCUOGLU <selim@tavux.tech>
2024-09-06 12:36:04 +0200

    Close previous opened tab before opening new tab

--- entrypoints/popup/popup.js
+++ entrypoints/popup/popup.js
@@ -1,10 +1,26 @@
 import {browser} from "wxt/browser";
 
-browser.tabs.create({
-    url: './main.html',
-}).then((tab) => {
-    console.log("Tab opened", tab);
-    window.close();
+browser.storage.local.get(['openedTabId'], function(result) {
+    const previousTabId = result.openedTabId;
+
+    if (previousTabId) {
+        browser.tabs.remove(previousTabId, function() {
+            // Ouvre un nouvel onglet après avoir fermé l'ancien
+            browser.tabs.create({ url: url }, function(newTab) {
+                // Stocke l'ID du nouvel onglet
+                browser.storage.local.set({ openedTabId: newTab.id });
+            });
+        });
+    }
+
+    browser.tabs.create({
+        url: './main.html',
+    }).then((newTab) => {
+        window.close();
+        browser.storage.local.set({ openedTabId: newTab.id });
+        console.log("Tab opened", newTab);
+    });
 });
+
 
 
