f2603074032b78a04dfc54179be3bb0d06cb5ed8
gamarcad <marcadet.gael@gmail.com>
2024-09-30 22:59:34 +0200

    Addition of the phrase interface in which words are shown and later guessed

--- entrypoints/initialisation/components/RecoveryPhrase.tsx
+++ entrypoints/initialisation/components/RecoveryPhrase.tsx
@@ -1,64 +1,101 @@
+import * as Carmentis from "../../../lib/carmentis-nodejs-sdk.js";
+import React, {useState} from "react";
+
+/**
+ * Generate a random number included between the two provided (included) bounds.
+ *
+ * @param min The lowest value the function can sample.
+ * @param max The highest value the function can sample.
+ *
+ * @return number A randomly chosen number between `min` and `max`.
+ */
+function randomIntFromInterval(min : number, max : number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+
+// generate a list of words
+const wordsList = Carmentis.generateWordList(12);
+
 export function RecoveryPhrase() {
+    // create a list of states
+    let wordStates = [];
+    let setWords = [];
+    for (let i = 0; i < 12; i++) {
+        let [word, setWord] = useState(wordsList[i]);
+        wordStates.push(word);
+        setWords.push(setWord);
+    }
+
+    let indexes = [];
+    for (let i = 0; i < wordsList.length; i++) {indexes.push(i);}
+
+
+    // generate three random numbers corresponding to the list of
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    do {
+        a = randomIntFromInterval(0, 11);
+        b = randomIntFromInterval(0, 11);
+        c = randomIntFromInterval(0, 11);
+    } while (a == b || a == b || a == c);
+
+
+    /**
+     * Event function executed when the user clicks on continue to start the challenge.
+     */
+    function startChallenge() {
+        // clear the inputs
+        for (let i = 0; i < 12; i++) {
+            if (i == a || i == b || i == c) {
+                setWords[i]("")
+            } else {
+                setWords[i](wordsList[i]);
+            }
+        }
+    }
+
+    /**
+     * Event function executed when a challenge word is modified.
+     */
+    function onChallengeWordsChanged(e) {
+        console.log("Challenge words changed", e)
+    }
+
+
     return (
         <>
             <h1 className="title justify-center align-content-center align-items-center flex">Save your passphrase</h1>
-            <p className="text-justify">
+            <p className="text-justify mb-8">
                 These words constitute your passphrase to recover your wallet in case you do not remember your password.
                 Your password is secret and should never be transmitted to another user.
 
                 Note these words carefully in a secure place.
             </p>
 
-            <div className="flex items-center justify-between align-items-center justify-content-center flex-col mb-4">
-                <div className="words-row">
-                    <div className="word-group">
-                        <label htmlFor=""></label>
-                        <input type="text" className="word-input"/>
-                    </div>
-                    <div className="word-group">
-                        <label htmlFor=""></label>
-                        <input type="text" className="word-input"/>
-                    </div>
-                    <div className="word-group">
-                        <label htmlFor=""></label>
-                        <input type="text" className="word-input"/></div>
-                </div>
-                <div className="words-row">
-                    <div className="word-group"><label htmlFor="">1</label><input type="text" className="word-input"/>
-                    </div>
-                    <div className="word-group"><label htmlFor="">2</label><input type="text" className="word-input"/>
-                    </div>
-                    <div className="word-group"><label htmlFor="">3</label><input type="text" className="word-input"/>
-                    </div>
-                </div>
-                <div className="words-row">
-                    <div className="word-group"><label htmlFor="">4</label><input type="text" className="word-input"/>
-                    </div>
-                    <div className="word-group"><label htmlFor="">5</label><input type="text" className="word-input"/>
-                    </div>
-                    <div className="word-group"><label htmlFor="">6</label><input type="text" className="word-input"/>
-                    </div>
-                </div>
-                <div className="words-row">
-                    <div className="word-group"><label htmlFor="">7</label><input type="text" className="word-input"/>
-                    </div>
-                    <div className="word-group"><label htmlFor="">8</label><input type="text" className="word-input"/>
-                    </div>
-                    <div className="word-group"><label htmlFor="">9</label><input type="text" className="word-input"/>
-                    </div>
-                </div>
-                <div className="words-row">
-                    <div className="word-group"><label htmlFor="">10</label><input type="text" className="word-input"/>
-                    </div>
-                    <div className="word-group"><label htmlFor="">11</label><input type="text" className="word-input"/>
-                    </div>
-                    <div className="word-group"><label htmlFor="">12</label><input type="text" className="word-input"/>
-                    </div>
-                </div>
+            <div className="flex  flex-wrap -mb-4">
+                {indexes.map(index => (
+                        <div className="word-group lg:w-1/3 md:w-1/3 mb-4 h-12">
+                            <label htmlFor={"w" + index}>{index + 1}</label>
+
+                            <input type="text"
+                                   id = {"w" + index}
+                                   readOnly={wordStates[index] != ""}
+                                   onChange={onChallengeWordsChanged}
+                                   value={wordStates[index]}
+                                   attr-set-word={setWords[index]}
+                                   className={"word-input " + (wordStates[index] === "" ? "border-green-400 ring-green-400" : "" )}
+                            />
+                        </div>
+                ))
+                }
             </div>
 
-            <button className="btn-primary btn-highlight">Continue</button>
-
+            <div className="row flex align-items-center justify-center align-content-center">
+                <div className="btn-primary btn-highlight" onClick={startChallenge}>Continue</div>
+            </div>
         </>
     );
 }
