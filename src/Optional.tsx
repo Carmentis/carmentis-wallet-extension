10e71804ca8b4771da73a36e7fcfbd45229c9b5f
gamarcad <marcadet.gael@gmail.com>
2024-10-21 17:36:00 +0200

    Addition of licences and minor comments

--- src/Optional.tsx
+++ src/Optional.tsx
@@ -1,13 +1,62 @@
-import {b} from "formdata-node/lib/File-cfd9c54a";
+/*
+ * Copyright (c) Carmentis. All rights reserved.
+ * Licensed under the Apache 2.0 licence.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
+/**
+ * The optional object is used to prevent the usage of undefined.
+ *
+ * In contrast with an undefined object, the optional is handled by most IDEs, meaning that
+ * when a developer uses an optional, he is warned that the object he is handling may be empty.
+ *
+ * Even more, if the developer attempts to access an undefined value, then the application crashes with
+ * a more interesting error message.
+ *
+ * Example:
+ * ```ts
+ * const valueOption = Optional.From(1);
+ * valueOption.isSome() // true
+ * valueOption.isEmpty() // false
+ * const value = valueOption.unwrap();
+ *
+ * const valueOption = Optional.Empty();
+ * valueOption.isSome() // false
+ * valueOption.isEmpty() // true
+ * const value = valueOption.unwrap(); // Error !
+ *
+ * ```
+ */
 export class Optional<T> {
 
+    /**
+     * The internal value handled by the option.
+     *
+     * @private
+     */
     private value : T | undefined;
+
 
     constructor( value ?: T ) {
         this.value = value;
     }
 
+    /**
+     * Returns the value inside the optional.
+     *
+     * @throws Error if the value is not defined.
+     */
     unwrap() : T {
         if ( this.value === undefined ) {
             throw new Error("Cannot unwrap an empty optional value!")
@@ -15,6 +64,12 @@ export class Optional<T> {
         return this.value;
     }
 
+
+    /**
+     * Returns the value inside the optional or a default value if it is not defined.
+     *
+     * @param defaultValue The default value to be returned if the internal value is undefined.
+     */
     unwrapOr( defaultValue : T ) : T {
         if ( this.value === undefined ) { return defaultValue }
         return this.value;
--- src/Wallet.tsx
+++ src/Wallet.tsx
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) Carmentis. All rights reserved.
+ * Licensed under the Apache 2.0 licence.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 import {Account, AccountData, EmailValidationProofData} from "@/src/Account.tsx";
 import {Encoders} from "@/src/Encoders.tsx";
 import {Optional} from "@/src/Optional.tsx";
@@ -8,6 +25,9 @@ const DEFAULT_NODE_ENDPOINT = "https://node.testapps.carmentis.io"
 const DEFAULT_DATA_ENDPOINT = "https://data.testapps.carmentis.io"
 const DEFAULT_WEBSOCKET_NODE_ENDPOINT = "wss://node.testapps.carmentis.io/websocket"
 
+/**
+ * Represents the internal structure of the wallet.
+ */
 export interface WalletData {
     seed : Array | undefined;
     counter : number;
