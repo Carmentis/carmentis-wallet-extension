d3eb4aca20928b8380664888b56322c577f3d546
gamarcad <marcadet.gael@gmail.com>
2024-10-28 11:51:07 +0100

    Incrementation of the version number.

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   manifest: {
     name: 'Carmentis Wallet',
-    version: '0.7.0',
+    version: '0.7.1',
     description: 'Carmentis Wallet',
     host_permissions: ['*://*/*'],
     browser_action: {},
