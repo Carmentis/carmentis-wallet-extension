4099995e5960404dc37aa40ef265b767a773fd4c
gamarcad <marcadet.gael@gmail.com>
2025-03-24 14:36:58 +0100

    bump to version 0.8.8

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   outDir: './.output',
   manifest: {
     name: 'Carmentis Wallet',
-    version: '0.8.7',
+    version: '0.8.8',
     description: 'Carmentis Wallet',
     host_permissions: ['*://*/*'],
     browser_action: {},
